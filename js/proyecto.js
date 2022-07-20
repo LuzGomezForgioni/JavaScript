56a327b79961c0da2ebe470062dde8b30a64eb68
Luz Gómez Forgioni <luzgomezforgioni@gmail.com>
2022-07-20 00:25:40 -0300

    Agregue total a la tabla

--- js/proyecto.js
+++ js/proyecto.js
@@ -3,23 +3,26 @@ let productos = [];
 let formulario;
 
 let inputProducto;
-let inputPrecio;
 let inputUnidad;
+let inputPrecio;
 let tabla;
+let total = 0;
+let cant = 0;
+
 
 class Producto {
-    constructor(producto, precio, unidad) {
+    constructor(producto, unidad, precio ) {
         this.producto = producto;
-        this.precio = precio;
         this.unidad = unidad;
+        this.precio = precio;
     }
 }
 
 function inicializarElementos() {
     formulario = document.getElementById("formulario");
     inputProducto = document.getElementById("producto");
-    inputPrecio = document.getElementById("precio");
     inputUnidad = document.getElementById("unidad");
+    inputPrecio = document.getElementById("precio");
     tabla = document.getElementById("tablaProductos");
 }
 inicializarElementos();
@@ -29,34 +32,32 @@ formulario.onsubmit = (event) => {
 
     let nuevoProducto = new Producto(
         inputProducto.value,
-        inputPrecio.value,
-        inputUnidad.value
+        inputUnidad.value,
+        inputPrecio.value
     );
     if (inputProducto.value != "" && inputUnidad.value != "" && inputPrecio.value != ""){
         productos.push(nuevoProducto);
-        productos.reverse();
-        limpiarTabla();
         agregarProductosTabla();
         formulario.reset();
     }
 };
 
-function limpiarTabla() {
-    while (tabla.rows.length > 1) {
-        tabla.deleteRow(1);
-    }
-}
-
 function agregarProductosTabla() {
-    productos.forEach((producto) => {
-        let tabla = document.querySelector(".tabla");
+        total = total + parseInt( (productos[productos.length - 1].precio) * productos[productos.length - 1].unidad )
+        cant = cant + parseInt(productos[productos.length - 1].unidad)
+
+        let tabla = document.getElementById("tabla");
         let filaTabla = document.createElement("tr");
 
         filaTabla.innerHTML = `
-        <td>${producto.producto} </td>
-        <td>${producto.precio} </td>
-        <td>${producto.unidad} </td>
+        <td>${productos[productos.length - 1].producto} </td>
+        <td>${productos[productos.length - 1].unidad} </td>
+        <td>${productos[productos.length - 1].precio} </td>
         `;
-        tabla.append(filaTabla);
-    });
-}
+
+        tabla.insertBefore(filaTabla, document.getElementById('tabla-total'));
+        document.getElementById('cant').innerText = `Cant.: ${cant}`
+
+        tabla.insertBefore(filaTabla, document.getElementById('tabla-total'));
+        document.getElementById('total').innerText = `$${total}`
+    };
